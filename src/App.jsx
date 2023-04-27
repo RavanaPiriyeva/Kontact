b47029a6620f0282ab0fe0b8fa4785c51973a542
RavanaPiriyeva <7j59c43@code.edu.az>
2023-04-27 12:30:29 +0400

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './home/Home'
-
+import Navbar from './Layout/Navbar/Navbar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Home />
+     <Navbar />
    </>
   )
 }
